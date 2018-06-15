c4300a14e61621d7064aae01b89f94ed1810d15e
Zehan Sunesara <usazehan@gmail.com>
2018-06-14 23:47:28 -0500

    Second Commit

--- src/App.js
+++ src/App.js
@@ -21,15 +21,26 @@ class App extends React.Component {
     const city = e.target.elements.city.value, country = e.target.elements.country.value;
     const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=`+ city +`,`+ country + `uk&appid=` + api_key + `&units=metric`);
     const data = await api_call.json();
-    console.log(data);
-    this.setState({
-      temperature: data.main.temp,
-      city: data.name,
-      country: data.sys.country,
-      humidity: data.main.humidity,
-      description: data.weather[0].description,
-      error: ""
-    });
+    if(city && country){ 
+      console.log(data);
+      this.setState({
+        temperature: data.main.temp,
+        city: data.name,
+        country: data.sys.country,
+        humidity: data.main.humidity,
+        description: data.weather[0].description,
+        error: ""
+      });
+    } else {
+      this.setState({
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: "Please enter the values."
+      });
+    }
   }
 
 
